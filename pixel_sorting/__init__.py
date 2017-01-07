4a381959a0d6cddcce4a5ceaede59af86b7a65c5
henne90gen <henne90gen@gmail.com>
2017-01-07 23:33:57 +0100

    Fixing broken imports again

--- pixel_sorting/__init__.py
+++ pixel_sorting/__init__.py
@@ -1,5 +1,5 @@
-from .SortCriteria import *
-from .helper import *
 from .PixelSorters import *
+from .SortCriteria import *
 from .Stencils import *
 from .art_factory import *
+from .helper import *
